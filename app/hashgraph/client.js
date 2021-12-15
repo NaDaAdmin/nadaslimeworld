c123d60fbab56abd73acddfebe91e018ed5984af
Won-Hyeong <Jmeding23@gmail.com>
2021-12-15 15:57:34 +0900

    - 로그 찍기

--- app/hashgraph/client.js
+++ app/hashgraph/client.js
@@ -176,6 +176,8 @@ class HashgraphClient extends HashgraphClientContract {
 		// Extract PV from encrypted
 		const privateKey = await Encryption.decrypt(encrypted_receiver_key)
 
+		console.log( privateKey )
+
 		// Associate with the token
 		await this.associateToAccount({
 			privateKey,
