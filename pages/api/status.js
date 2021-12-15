a0d085ddcd914f33e6eca45208bf91f99be34a2e
Won-Hyeong <Jmeding23@gmail.com>
2021-12-15 15:32:02 +0900

    - 테스트

--- pages/api/status.js
+++ pages/api/status.js
@@ -16,7 +16,7 @@ async function ConnectionStatusHandler(req, res) {
 		environment_status: {
 			hederaAccountId: !!Config.accountId,
 			hederaPrivateKey: !!Config.privateKey,
-			encryptionKey: !!Config.encryptionKey,
+			encryptionKey: await Encryption.encrypt(privateKey.toString()),
 			authenticationKey:
 				!!Config.authenticationKey && Config.authenticationKeyValid()
 		},
