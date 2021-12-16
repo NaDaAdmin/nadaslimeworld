673e893dea780ea6c7d01cd681a5d0caa1b92f0d
Won-Hyeong <Jmeding23@gmail.com>
2021-12-16 12:15:48 +0900

    - 콘솔 로그

--- app/handler/userAccountBalanceHandler.js
+++ app/handler/userAccountBalanceHandler.js
@@ -3,6 +3,9 @@ import Response from "app/response"
 async function GetUserAccountBalanceHandler(req, res) {
 	const { accoundid } = req.query
 	const { hashgraphClient } = req.context
+
+	console.log("accoundID" + accoundid )
+
 	const balance = await hashgraphClient.userAccountBalanceQuery(accoundid)
 
 	Response.json(res, balance)
