aaf814846a4d5ff35d242a94e1a659367a3e625e
Won-Hyeong <Jmeding23@gmail.com>
2021-12-22 15:03:45 +0900

    - 로그 추가.

--- app/handler/createSmartContractHandler.js
+++ app/handler/createSmartContractHandler.js
@@ -3,12 +3,14 @@ import Response from "app/response"
 
 async function CreateSmartContractHandler(req, res) {
 
+	console.log("-----------------1");
 	const validationErrors = createSmartContract(req.body)
 
 	if (validationErrors) {
 		return Response.unprocessibleEntity(res, validationErrors)
 	}
 
+	console.log("-----------------2");
 
 	const { encrypted_receiver_key, gas, file_id } = req.body
 	const contractInfo = {
@@ -22,6 +24,8 @@ async function CreateSmartContractHandler(req, res) {
 	if (recvResponse) {
 		return Response.json(res, recvResponse)
 	}
+
+	console.log("-----------------3");
 
 	// This has to be bolstered up with correct error handling
 	return Response.badRequest(res)
