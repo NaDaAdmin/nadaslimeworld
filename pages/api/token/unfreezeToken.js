b3f3ceb540bbc72a42027414d0434cd1048ffc8f
Won-Hyeong <Jmeding23@gmail.com>
2021-12-27 16:57:17 +0900

    - 토큰 동결 해제 기능

--- pages/api/token/unfreezeToken.js
+++ pages/api/token/unfreezeToken.js
@@ -1,9 +1,9 @@
 import onlyPost from "app/middleware/onlyPost"
 import useHashgraphContext from "app/context/useHashgraphContext"
 import prepare from "app/utils/prepare"
-import FreezeTokenHandler from "app/handler/freezeTokenHandler"
+import UnfreezeTokenHandler from "app/handler/unfreezeTokenHandler"
 
 export default prepare(
 	onlyPost,
 	useHashgraphContext
-)(FreezeTokenHandler)
+)(UnfreezeTokenHandler)
