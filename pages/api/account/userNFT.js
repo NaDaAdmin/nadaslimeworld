e0ce93f7ee74162aedad09ded8c06c4e2656b83b
grakung <client@nadadigital.co.kr>
2022-03-08 15:00:28 +0900

    userNFT get 형식으로 변경

--- pages/api/account/userNFT.js
+++ pages/api/account/userNFT.js
@@ -1,11 +1,9 @@
 import onlyGet from "app/middleware/onlyGet"
-import withAuthentication from "app/middleware/withAuthentication"
 import useHashgraphContext from "app/context/useHashgraphContext"
 import prepare from "app/utils/prepare"
 import GetUserAccountNFTHandler from "app/handler/userAccountNFTHandler"
 
 export default prepare(
 	onlyGet,
-	withAuthentication,
 	useHashgraphContext
 )(GetUserAccountNFTHandler)
