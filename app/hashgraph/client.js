15ba26930df5803ad10730cf4a7d21c9fa587fe2
grakung <client@nadadigital.co.kr>
2022-04-11 14:27:15 +0900

    KeyList Import

--- app/hashgraph/client.js
+++ app/hashgraph/client.js
@@ -37,6 +37,7 @@ import Explorer from "app/utils/explorer"
 import sendWebhookMessage from "app/utils/sendWebhookMessage"
 import Specification from "app/hashgraph/tokens/specifications"
 import {IpfsAPI} from "ipfs-api"
+import { KeyList } from "@hashgraph/sdk/lib/exports"
 
 
 class HashgraphClient extends HashgraphClientContract {
