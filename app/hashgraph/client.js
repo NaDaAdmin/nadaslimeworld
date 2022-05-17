11659efd509cb2da0474b333311119373830c2da
NaDaAdmin <client@nadadigital.co.kr>
2022-05-17 22:13:24 +0900

    nft test

--- app/hashgraph/client.js
+++ app/hashgraph/client.js
@@ -432,9 +432,9 @@ class HashgraphClient extends HashgraphClientContract {
 		let transaction = await new TransferTransaction()
 			.addTokenTransfer(token_id1, account_id1, -(adjustedAmountBySpec))
 			.addTokenTransfer(token_id1, account_id2, adjustedAmountBySpec)
-			.addNftTransfer(new NftId(token_id2, 1), account_id2, account_id1)
-			// .addTokenTransfer(token_id2, account_id2, -1)
-			// .addTokenTransfer(token_id2, account_id1, 1)
+			//.addNftTransfer(new NftId(token_id2, 1), account_id2, account_id1)
+			.addTokenTransfer(token_id2, account_id2, -1)
+			.addTokenTransfer(token_id2, account_id1, 1)
 			.freezeWith(client);
 
 
