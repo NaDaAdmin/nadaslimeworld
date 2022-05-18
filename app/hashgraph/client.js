50112dac327e49b120786e8c0bed26a192d195ec
NaDaAdmin <client@nadadigital.co.kr>
2022-05-18 18:29:49 +0900

    nft test

--- app/hashgraph/client.js
+++ app/hashgraph/client.js
@@ -376,8 +376,9 @@ class HashgraphClient extends HashgraphClientContract {
 			.addTokenTransfer(token_id1, account_id1, -(adjustedAmountBySpec))
 			.addTokenTransfer(token_id1, account_id2, adjustedAmountBySpec)
 			.addNftTransfer(token_id2, serialNum, account_id2, account_id1)
+			.freezeWith(client)
 			.sign(PrivateKey.fromString(Config.privateKey));
-			
+
 		console.log("transaction");
 
 		//Schedule a transaction
