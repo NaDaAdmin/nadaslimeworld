8ba4a8d2fad25e3d9c97653fc2f4b2d6b8b4f02a
NaDaAdmin <client@nadadigital.co.kr>
2022-05-19 20:00:13 +0900

    nft test

--- app/hashgraph/client.js
+++ app/hashgraph/client.js
@@ -368,8 +368,8 @@ class HashgraphClient extends HashgraphClientContract {
 			.execute(client)
 
 		const token = JSON.parse(tokens.toString())[token_id1]
-		//const adjustedAmountBySpec = amount * 10 ** specification.decimals
-		const adjustedAmountBySpec = amount
+		const adjustedAmountBySpec = amount * 10 ** specification.decimals
+		//const adjustedAmountBySpec = amount
 		
 		if (token < adjustedAmountBySpec) {
 			return false
